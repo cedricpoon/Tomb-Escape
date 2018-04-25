58bb23027dcdb3dd518ab7bb7524923e9384184e
Benjamin <lnf19970311@gmail.com>
2018-04-26 03:02:55 +0800

    volume can be adjusted
    the bar is working now

---
+++ Assets/VolumeAdjust.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeAdjust : MonoBehaviour {
+
+	public Slider Volume;
+
+	//public AudioSource volumeAudio;
+ 	public void VolumeController(){
+     	AudioListener.volume = Volume.value;
+ 	}
+}
