2561297556871c7be0a862f3e49d9a69603c02d4
ron.ky <ron.ky@gmail.com>
2018-04-26 03:07:34 +0800

    add scene materials, mini-map

---
+++ Assets/Resources/Scripts/Camera/MiniCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Game/MiniCamera")]
+public class MiniCamera : MonoBehaviour {
+
+	private Camera cam;
+	// Use this for initialization
+	void Start () {
+		float ratio = (float)Screen.width / (float)Screen.height;
+		this.GetComponent<Camera>().rect = new Rect ((1 - 0.2f), (1 - 0.2f * ratio), 0.2f, 0.2f * ratio);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
