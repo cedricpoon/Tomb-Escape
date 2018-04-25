f83f84689c65af75b0c3db9c51660e16c15f89d2
Cedric Poon <cedric@poons.me>
2018-04-26 05:50:00 +0800

    Add potion and adopt new library

---
+++ Assets/Resources/Scripts/Items/Potion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : Attachable {
+
+	protected override void Start ()
+	{
+		base.Start ();
+
+		HasTrigger = true;
+	}
+
+	public override void Trigger ()
+	{
+		base.Trigger ();
+
+		player.GetComponent<Health> ().Heal ();
+		base.Corrupt ("Used");
+	}
+}
