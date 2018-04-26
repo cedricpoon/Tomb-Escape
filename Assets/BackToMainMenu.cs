630d8bea1bef33b63f7d6bdc1054352591eb8970
Benjamin <lnf19970311@gmail.com>
2018-04-26 11:44:41 +0800

    added back to main menu when die
    added back to main menu when die

---
+++ Assets/BackToMainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackToMainMenu : MonoBehaviour {
+
+
+	public void GoBack(){
+		Debug.Log("Back to Main Menu");
+		Application.LoadLevel("Menu");
+		
+	}
+	
+}
